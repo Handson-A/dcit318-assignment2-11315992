ff2ce0861863cc4d300bb1c9d6f7e37668907044
Handson <169119935+Handson-A@users.noreply.github.com>
2025-08-06 17:41:30 +0000

    Task2
    C# assignment. trying out abstract classes and methods using area of a circle and rectangle

---
+++ AbstractClassAndMethod/Task2.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace AbstractClassAndMethod{
+    // Abstract base class
+    abstract class Shape
+    {
+        public abstract double GetArea();
+    }
+
+    // Circle class
+    class Circle : Shape
+    {
+        public double Radius { get; set; }
+
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+
+        public override double GetArea()
+        {
+            return Math.PI * Radius * Radius;
+        }
+    }
+
+    // Rectangle class
+    class Rectangle : Shape
+    {
+        public double Width { get; set; }
+        public double Height { get; set; }
+
+        public Rectangle(double width, double height)
+        {
+            Width = width;
+            Height = height;
+        }
+
+        public override double GetArea()
+        {
+            return Width * Height;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Shape circle = new Circle(7); // assigned
+            Shape rectangle = new Rectangle(4, 7); //assigned testing
+
+            Console.WriteLine("Circle Area: " + circle.GetArea().ToString("F1") + " square units");
+            Console.WriteLine("Rectangle Area: " + rectangle.GetArea().ToString("F1") + " square units");
+        }
+    }
+}
