21589039b5639a13893e338ab4af4390c97493f9
Handson <169119935+Handson-A@users.noreply.github.com>
2025-08-06 17:54:33 +0000

    Task3
    Implementing interfaces in c#, with 2 classes

---
+++ Interfaces/Task3.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Interface
+{
+    // Interface definition
+    interface IMovable
+    {
+        void Move();
+    }
+
+    // Class implementing IMovable
+    class Car : IMovable
+    {
+        public void Move()
+        {
+            Console.WriteLine("Car is moving");
+        }
+    }
+
+    // Another class implementing IMovable
+    class Bicycle : IMovable
+    {
+        public void Move()
+        {
+            Console.WriteLine("Bicycle is moving");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IMovable car = new Car();
+            IMovable bicycle = new Bicycle();
+
+            car.Move();
+            bicycle.Move();
+        }
+    }
+}
